300d9e9542c8c5d16e27d15be70b169a58642992
gabrielmussafiri <musgabriel2@gmail.com>
2025-02-28 19:06:01 +0200

    udpated

--- main.js
+++ main.js
@@ -1,62 +1,60 @@
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn =document.querySelector('#close-btn');
-const menu=document.querySelector('nav .container ul');
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const menu = document.querySelector("nav .container ul");
+const navItems = menu.querySelectorAll("li");
 
 // show menu
-menuBtn.addEventListener('click',()=>{
-    menu.style.display='block';
-    menuBtn.style.display='none';
-    closeBtn.style.display='inline-block';
-})
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "block";
+  menuBtn.style.display = "none";
+  closeBtn.style.display = "inline-block";
+});
 // hide menu
-closeBtn.addEventListener('click', ()=>{
-    menu.style.display='none';
-    menuBtn.style.display='inline-block';
-    closeBtn.style.display='none';
-})
+closeBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  menuBtn.style.display = "inline-block";
+  closeBtn.style.display = "none";
+});
 
-const navItems= menu.querySelectorAll('li')
+const changeActiveItem = () => {
+  navItems.forEach((item) => {
+    const link = item.querySelector("a");
+    link.classList.remove("active");
+  });
+};
 
-const changeActiveItem=()=>{
-    navItems.forEach(item=>{
-     const link=item.querySelector('a');
-     link.classList.remove('active');
-    })
-}
-
-navItems.forEach(item=>{
-    const link=item.querySelector('a');
-    link.addEventListener('click',()=>{
-        changeActiveItem();
-        link.classList.add('active');
-
-    })
-})
+navItems.forEach((item) => {
+  const link = item.querySelector("a");
+  link.addEventListener("click", () => {
+    changeActiveItem();
+    link.classList.add("active");
+  });
+});
 
 // red more about
-const readMoreBtn = document.querySelector('.read-more');
-const readMoreContent=document.querySelector('.read-more-content');
+const readMoreBtn = document.querySelector(".read-more");
+const readMoreContent = document.querySelector(".read-more-content");
 
-readMoreBtn.addEventListener('click',()=>{
-    readMoreContent.classList.toggle('show-content');
-    if(readMoreContent.classList.contains('show-content')){
-        readMoreBtn.textContent='Show less'
-    }
-    else{
-        readMoreBtn.textContent='Show more'
-
-    }
-})
+readMoreBtn.addEventListener("click", () => {
+  readMoreContent.classList.toggle("show-content");
+  if (readMoreContent.classList.contains("show-content")) {
+    readMoreBtn.textContent = "Show less";
+  } else {
+    readMoreBtn.textContent = "Show more";
+  }
+});
 // show or hide skills items
-const skillsItems=document.querySelectorAll('section.skills .skill')
+const skillsItems = document.querySelectorAll("section.skills .skill");
 
-skillsItems.forEach(skill=>{
-    skill.querySelector('.head').addEventListener('click',()=>{
-        skill.querySelector('.items').classList.toggle('show-items')
-    })
-})
+skillsItems.forEach((skill) => {
+  skill.querySelector(".head").addEventListener("click", () => {
+    skill.querySelector(".items").classList.toggle("show-items");
+  });
+});
 
 //add box shadow nav bar
-window.addEventListener('scroll',()=>{
-    document.querySelector('nav').classList.toggle('show-box-shadow',window.scrollY>0)
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("show-box-shadow", window.scrollY > 0);
+});
